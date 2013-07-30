7a9b44d30ee37e789da669a5c38ca04fb9f296bc
Luke Shimanuki <skywalker3.14159@gmail.com>
2013-07-30 16:28:09 -0700

    idk what i changed, but ai doesn't seg fault on depth 0 or bad alloc on 3

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,26 @@
 int main (int argc, char **argv)
 {
 	game g;
-	g.set_type(0,0);
 	g.initialize(1);
-	if (argc >= 2)
+	if (argc >= 3)
 	{
-		g.cp[0].set_depth(argv[1][0] - 48);
-		g.cp[1].set_depth(argv[1][0] - 48);
+		g.set_type(argv[1][0] - 48, argv[2][0] - 48);
+	}
+	else
+	{
+		g.set_type(1, 1);
+
+	}
+	if (argc >= 4)
+	{
+		g.cp[0].set_depth(argv[3][0] - 48);
+		g.cp[1].set_depth(argv[3][0] - 48);
+	}
+	else
+	{
+		g.cp[0].set_depth(5);
+		g.cp[1].set_depth(5);
 	}
 	g.set_log("xiangqi.log");
 	g.play();
-/*	std::vector<int> s = g.cp[0].choose_move(g.pos, 0);
-	textio tio;
-	g.pos.move(s);
-	tio.draw(g.pos);
-*/
 }
